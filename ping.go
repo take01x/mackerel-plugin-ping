e294fd5d0a356878e0c82a46521d520b173ecdba
Takehiro YOSHIHAMA <take001x@gmail.com>
2018-02-10 16:25:20 +0900

    Add group option and change to use `go-mackerel-plugin`

--- ping.go
+++ ping.go
@@ -9,22 +9,25 @@ import (
 	"strings"
 	"time"
 
-	mp "github.com/mackerelio/go-mackerel-plugin-helper"
+	mp "github.com/mackerelio/go-mackerel-plugin"
 	fping "github.com/tatsushid/go-fastping"
 )
 
 type PingPlugin struct {
+	Prefix      string
 	Hosts       []string
 	Labels      []string
+	Groups      map[string]string
 	Tempfile    string
 	Count       int
 	WaitTime    int
 	AcceptCount int
 	SourceIP    string
+	Stacked	    bool
 }
 
-func (pp PingPlugin) FetchMetrics() (map[string]interface{}, error) {
-	stat := make(map[string]interface{})
+func (pp PingPlugin) FetchMetrics() (map[string]float64, error) {
+	stat := make(map[string]float64)
 	total := make(map[string]float64)
 	count := make(map[string]int)
 
@@ -63,101 +66,128 @@ func (pp PingPlugin) FetchMetrics() (map[string]interface{}, error) {
 	return stat, nil
 }
 
-func (pp PingPlugin) GraphDefinition() map[string](mp.Graphs) {
-	metrics := []mp.Metrics{}
+func (pp PingPlugin) GraphDefinition() map[string]mp.Graphs {
+	metrics := map[string][]mp.Metrics{}
 	for i := 0; i < len(pp.Hosts); i++ {
-		metrics = append(metrics, mp.Metrics{
-			Name:    escapeHostName(pp.Hosts[i]),
+		name := escapeHostName(pp.Hosts[i])
+		key := "rtt"
+		if pp.Groups[name] != ":def" && pp.Groups[name] != "" {
+			key = fmt.Sprintf("%s.%s", key, pp.Groups[name])
+		}
+		metrics[key] = append(metrics[key], mp.Metrics{
+			Name:    name,
 			Label:   pp.Labels[i],
 			Diff:    false,
-			Stacked: false,
+			Stacked: pp.Stacked,
 		})
+		if os.Getenv("MACKEREL_AGENT_PLUGIN_META") != "" && key != "rtt" {
+			metrics["rtt.#"] = append(metrics["rtt.#"], mp.Metrics{
+				Name:    name,
+				Label:   pp.Labels[i],
+				Diff:    false,
+				Stacked: pp.Stacked,
+			})
+		}
 	}
 
-	return map[string](mp.Graphs){
-		"ping.rtt": mp.Graphs{
-			Label:   "Ping Round Trip Times",
-			Unit:    "float",
-			Metrics: metrics,
-		},
+	graphs := map[string]mp.Graphs{}
+	for k, v := range metrics {
+		label := "Ping Round Trip Times"
+		if k != "rtt.#" && k != "rtt" {
+			label = fmt.Sprintf("%s (Group: %s)", label, strings.SplitN(k, ".", 2)[1])
+		} else if k == "rtt" {
+			label = fmt.Sprintf("%s (Group: default)", label)
+		}
+		graphs[k] = mp.Graphs{
+			Label:    label,
+			Unit:     "float",
+			Metrics:  v,
+		}
 	}
+
+	return graphs
+}
+
+func (pp PingPlugin) MetricKeyPrefix() string {
+	if pp.Prefix == "" {
+		pp.Prefix = "ping"
+	}
+	return pp.Prefix
 }
 
 func escapeHostName(host string) string {
 	return strings.Replace(strings.Replace(host, ".", "_", -1), ":", "_", -1)
 }
 
-func validate(ipAddr string) bool {
-	r := regexp.MustCompile("^\\d+\\.\\d+\\.\\d+\\.\\d+$")
-	return r.MatchString(ipAddr)
-}
-
-func parseHostsString(optHost string, ipv6 bool, strict ...string) ([]string, []string, error) {
+func parseHostsString(optHost string, ipv6 bool, strict ...string) ([]string, map[string]string, []string, error) {
 	hosts := strings.Split(optHost, ",")
-	ips, labels := make([]string, len(hosts)), make([]string, len(hosts))
+	ips, groups, labels := make([]string, len(hosts)), make(map[string]string), make([]string, len(hosts))
 
 	for i := 0; i < len(hosts); i++ {
-		v := strings.SplitN(hosts[i], ":", 2)
-		if strings.Count(hosts[i], ":") >= 2 {
-			r := regexp.MustCompile(`\[(.*)\](.*)`)
-			v = []string{r.ReplaceAllString(hosts[i], "$1"), strings.Replace(r.ReplaceAllString(hosts[i], "$2"), ":", "", 1)}
-		}
+		v := strings.SplitN(hosts[i], ":", 3)
+		version := "ip4"
 		if ipv6 {
-			ip, err := net.ResolveIPAddr("ip6", v[0])
-			if err != nil {
-				if strict[0] != "" {
-					return nil, nil, err
-				}
-				continue
-			}
-			ips[i] = ip.String()
-		} else {
-			if !validate(v[0]) {
-				ip, err := net.ResolveIPAddr("ip4", v[0])
-				if err != nil {
-					if strict[0] != "" {
-						return nil, nil, err
-					}
-					continue
-				}
-				ips[i] = ip.String()
-			} else {
-				ips[i] = v[0]
+			version = "ip6"
+			if strings.Count(hosts[i], "[") == 1 {
+				r := regexp.MustCompile(`\[(.*)\](:.*)(:.*)`)
+				v1 := strings.Replace(r.ReplaceAllString(hosts[i], "$2"), ":", "", 1)
+				v2 := strings.Replace(r.ReplaceAllString(hosts[i], "$3"), ":", "", 1)
+				v = []string{r.ReplaceAllString(hosts[i], "$1"), v1, v2}
 			}
 		}
+		ip, err := net.ResolveIPAddr(version, v[0])
+		if err != nil {
+			if strict[0] != "" {
+				return nil, nil, nil, err
+			}
+			continue
+		}
+		ips[i] = ip.String()
+		labels[i] = v[0]
+		groups[escapeHostName(ips[i])] = ":def"
 		if len(v) == 2 && v[1] != "" {
 			labels[i] = v[1]
-		} else {
-			labels[i] = v[0]
+		} else if len(v) == 3 {
+			if v[1] != "" {
+				groups[escapeHostName(ips[i])] = v[1]
+			}
+			if v[2] != "" {
+				labels[i] = v[2]
+			}
 		}
 	}
 
-	return ips, labels, nil
+	return ips, groups, labels, nil
 }
 
 func main() {
-	optHost := flag.String("host", "127.0.0.1:localhost", "IP Address[:Metric label],IP[:Label],...")
+	optHost := flag.String("host", "127.0.0.1:localhost", "IPv4 Address[[:Group]:Metric Label],[IPv6 Address]:[[:Group]:Label],FQDN[[:Group]:Label],...")
 	optTempfile := flag.String("tempfile", "", "Temp file name")
 	optCount := flag.Int("count", 1, "Sending (and receiving) count ping packets.")
 	optWaitTime := flag.Int("waittime", 1000, "Wait time, Max RTT(ms)")
 	optAcceptCount := flag.Int("acceptmiss", 0, "Accept out of wait time count ping packets.")
 	optIPv6 := flag.Bool("6", false, "Enable IPv6.")
 	optSourceIP := flag.String("source", "", "Source IP Address. If the IP Address is invalid, it will be ignored.")
+	optPrefix := flag.String("prefix", "", "Prefix of graph metrics.")
+	optStacked := flag.Bool("stacked", false, "Use Stacked graph.")
 	flag.Parse()
 
-	hosts, labels, err := parseHostsString(*optHost, *optIPv6, os.Getenv("MACKEREL_AGENT_PLUGIN_META"))
+	hosts, groups, labels, err := parseHostsString(*optHost, *optIPv6, os.Getenv("MACKEREL_AGENT_PLUGIN_META"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err.Error())
 		os.Exit(1)
 	}
 
 	var pp PingPlugin
+	pp.Prefix = *optPrefix
 	pp.Hosts = hosts
+	pp.Groups = groups
 	pp.Labels = labels
 	pp.Count = *optCount
 	pp.WaitTime = *optWaitTime
 	pp.AcceptCount = *optAcceptCount
 	pp.SourceIP = *optSourceIP
+	pp.Stacked = *optStacked
 
 	helper := mp.NewMackerelPlugin(pp)
 
